470abb14e12af3f3ece95c368567e13d2aea7847
Diego Torres <diegotorres50@hotmail.com>
2017-09-02 11:33:35 -0300

    asset-management, sumando estilos a la aplicacion

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // Para convertir archivos css a la grafica de dependencias usando un cargador.
+  // npm install --save-dev style-loader css-loader
+  // ver: https://webpack.js.org/guides/asset-management/
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
